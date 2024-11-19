3396b7aaee08974a830ccf1458fdd21e86b6b515
michaelhaaf <michael.haaf@gmail.com>
2024-11-19 12:32:01 -0500

    attempt to make the pptx conversion stuff a bit more portable, incomplete

---
+++ misc/setup.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# TODO environment set up to make this dead simple, might generalize
+# Something like:
+
+set -euo pipefail
+
+# python -m ensurepip --upgrade
+# python -m pip install --upgrade pip
+# python install -r requirements.txt
